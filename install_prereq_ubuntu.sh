afb22c2af4b8ee1c19fe43823b5ce3a3a56396d7
EgorNuzhnykh <nuzhnykhev@gmail.com>
2019-05-03 10:30:43 +0300

    [all] initial commit

---
+++ install_prereq_ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install Powershell
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+sudo apt-get update
+sudo apt-get install -y powershell
+
+# Install ansible
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+# Install az cli
+sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+sudo apt-get update
+sudo apt-get install azure-cli
